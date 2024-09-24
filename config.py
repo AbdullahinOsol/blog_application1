f4ac2602340fbe2cdb79ce173e91704877975ee9
AbdullahinOsol <abdullah.arshad@osoltech.com>
2024-09-24 12:44:49 +0500

    Configuration changes

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
     MAIL_USE_SSL = True
